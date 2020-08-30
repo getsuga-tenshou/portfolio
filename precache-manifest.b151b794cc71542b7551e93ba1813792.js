d85cdfc9e65a6b76608291305b35bcb508d06bbd
Chandan Saini <47878598+getsuga-tenshou@users.noreply.github.com>
2020-08-30 20:31:29 +0530

    Updates

---
+++ precache-manifest.b151b794cc71542b7551e93ba1813792.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bb558e6932e120de84512c8e8b1b07",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d09af4f99d8bd2e57ddd",
+    "url": "/portfolio/static/css/main.b20e5300.chunk.css"
+  },
+  {
+    "revision": "10ac8b45caa8e6bd940b",
+    "url": "/portfolio/static/js/2.d5b8aa46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.d5b8aa46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09af4f99d8bd2e57ddd",
+    "url": "/portfolio/static/js/main.f46e4f39.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9d07141dd9a4b776147d16a109d78dff",
+    "url": "/portfolio/static/media/facebook.9d07141d.png"
+  },
+  {
+    "revision": "1b9b9f71269e504156ce9d89a00f2551",
+    "url": "/portfolio/static/media/github-image.1b9b9f71.png"
+  },
+  {
+    "revision": "dcf080ab2119b6c9824074eeefdc290c",
+    "url": "/portfolio/static/media/main-image.dcf080ab.jpg"
+  },
+  {
+    "revision": "7bfb030875fafaf609c5a15e65bd6a9c",
+    "url": "/portfolio/static/media/project.7bfb0308.png"
+  },
+  {
+    "revision": "d45375da32a8263c260d22f64c8240c4",
+    "url": "/portfolio/static/media/resume.d45375da.png"
+  },
+  {
+    "revision": "8f35a40403a84631c4125c4f1859c7a6",
+    "url": "/portfolio/static/media/twitter.8f35a404.png"
+  }
+]);
